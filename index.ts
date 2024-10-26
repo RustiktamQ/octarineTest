4ab18a8e0f4e2f01f2b7460072f5cf6b19c0f082
RustiktamQ <rramilperm@gmail.com>
2024-10-27 00:39:39 +0500

    added offset calc

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ class BlackSquare {
     private position: Rectangle;
 	private isDragging: boolean = false;
 	private color: Color;
+	private offset: Vector2;
 
     constructor(x: number, y: number, Width: number, Height: number, fillColor: Color) {
         this.position = new Rectangle();
@@ -21,7 +22,10 @@ class BlackSquare {
         this.position.Width = Width;
         this.position.Height = Height;
 		this.color = fillColor;
-
+		this.offset = new Vector2(
+			0,
+			0
+		);
 		
 		InputEventSDK.on("MouseKeyDown", this.onMouseKeyDown.bind(this));
         InputEventSDK.on("MouseKeyUp", this.onMouseKeyUp.bind(this));
@@ -31,8 +35,8 @@ class BlackSquare {
         if (this.isDragging) {
             const mousePos = Input.CursorOnScreen;
 			console.log(mousePos.x);
-            this.position.x = mousePos.x;
-            this.position.y = mousePos.y;
+            this.position.x = mousePos.x - this.offset.x;
+            this.position.y = mousePos.y - this.offset.y;
         }
 
         RendererSDK.FilledRect(this.position.pos1, this.position.Size, this.color);
@@ -43,13 +47,16 @@ class BlackSquare {
             const mousePos = Input.CursorOnScreen;
             if (this.isMouseClickedInSquare(mousePos)) {
                 this.isDragging = true;
+                this.offset = new Vector2(
+                    mousePos.x - this.position.x,
+                    mousePos.y - this.position.y
+                );
             }
         }
     }
 
     private onMouseKeyUp(key: VMouseKeys) {
         if (key === VMouseKeys.MK_LBUTTON) {
-			const mousePos = Input.CursorOnScreen;
             this.isDragging = false;
         }
     }
