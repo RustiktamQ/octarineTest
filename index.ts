0d1bd6ee2884d982dfa091041b73d66af9c38f32
RustiktamQ <rramilperm@gmail.com>
2024-10-27 00:54:45 +0500

    bug fix

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ class BlackSquare {
     private position: Rectangle;
 	private isDragging: boolean = false;
 	private color: Color;
+	private bornColor: Color;
 	private offset: Vector2;
 
     constructor(x: number, y: number, Width: number, Height: number, fillColor: Color) {
@@ -22,6 +23,7 @@ class BlackSquare {
         this.position.Width = Width;
         this.position.Height = Height;
 		this.color = fillColor;
+		this.bornColor = fillColor;
 		this.offset = new Vector2(
 			0,
 			0
@@ -34,7 +36,6 @@ class BlackSquare {
     public Draw(otherSquare?: BlackSquare) {
         if (this.isDragging) {
             const mousePos = Input.CursorOnScreen;
-			console.log(mousePos.x);
             this.position.x = mousePos.x - this.offset.x;
             this.position.y = mousePos.y - this.offset.y;
         }
@@ -43,8 +44,7 @@ class BlackSquare {
             this.color = Color.Red;
             otherSquare.color = Color.Red;
         } else {
-            this.color = Color.Black;
-            if (otherSquare) otherSquare.color = Color.White;
+            this.color = this.bornColor;
         }
 
         RendererSDK.FilledRect(this.position.pos1, this.position.Size, this.color);
