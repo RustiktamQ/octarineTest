c68066a5570c79dfc783f18c0cb84a6b5059745b
RustiktamQ <rramilperm@gmail.com>
2024-10-26 23:37:44 +0500

    try click in square

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
-import { EventsSDK, Color, Rectangle, RendererSDK } from "github.com/octarine-public/wrapper/index";
+import { 
+	Vector2, 
+	Input, 
+	VMouseKeys, 
+	InputEventSDK, 
+	EventsSDK, 
+	Color, 
+	Rectangle, 
+	RendererSDK 
+} from "github.com/octarine-public/wrapper/index";
 
 class BlackSquare {
     private position: Rectangle;
@@ -9,12 +18,27 @@ class BlackSquare {
         this.position.y = 100;
         this.position.Width = 200;
         this.position.Height = 200;
+		
+		InputEventSDK.on("MouseKeyDown", this.onMouseKeyDown.bind(this));
     }
 
     public Draw() {
         RendererSDK.FilledRect(this.position.pos1, this.position.Size, Color.Black);
     }
+
+	private onMouseKeyDown(key: VMouseKeys) {
+		if (key === VMouseKeys.MK_LBUTTON) {
+			const mousePos = Input.CursorOnScreen;
+			if (this.isMouseClickedInSquare(mousePos)) {
+				console.log("clicked in square");
+			}
+		}
+	}
+
+	private isMouseClickedInSquare(mousePos: Vector2): boolean {
+        return mousePos.IsUnderRectangle(this.position.x, this.position.y, this.position.Width, this.position.Height);
+    }
 }
 
-const blackSquare = new BlackSquare();
-EventsSDK.on("Draw", () => blackSquare.Draw());
+const clickableSquare = new BlackSquare();
+EventsSDK.on("Draw", () => clickableSquare.Draw());
