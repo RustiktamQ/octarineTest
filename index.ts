84d6660cdcd33f1fa87c75d945520c62d3940ce7
RustiktamQ <rramilperm@gmail.com>
2024-10-26 22:42:04 +0500

    test changes

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { EventsSDK } from "github.com/octarine-public/wrapper/index"
 
-console.log("Hello world!")
+console.log("Hello world!123")
 EventsSDK.on("GameStarted", () => {
 	console.log("Hello world!")
 })
